b2e8524d244ec9f552fedb074306b8bd2db6af99
arandombit <a.random.luis@proton.me>
2025-10-02 16:37:39 -0400

    add DigitalOcean droplet resource with Docker provisioning

--- main.tf
+++ main.tf
@@ -15,7 +15,6 @@ variable "digitalocean_ssh_key_name" { type = string }
 variable "digitalocean_token" { type = string }
 variable "private_key" { type = string }
 variable "public_key" { type = string }
-
 variable "domain_name" { type = string }
 
 variable "domain" {
@@ -35,3 +34,26 @@ provider "digitalocean" { token = var.digitalocean_token }
 provider "docker" { host = "unix:///var/run/docker.sock" }
 
 data "digitalocean_ssh_key" "terraform" { name = var.digitalocean_ssh_key_name }
+
+resource "digitalocean_droplet" "web" {
+  image = "debian-13-x64"
+  name = "web"
+  private_networking = true
+  region = "nyc3"
+  size = "s-1vcpu-1gb"
+  ssh_keys = [data.digitalocean_ssh_key.terraform.id]
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.private_key)
+    timeout = "2m"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt-get update",
+      "sudo apt-get install -y python3 docker.io docker-compose",
+      "mkdir app"
+    ]
+  }
+}
