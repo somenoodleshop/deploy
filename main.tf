70ad4366e6c1c344e76f8e29eecf65edb90a149e
arandombit <a.random.luis@proton.me>
2025-12-16 03:26:46 -0500

    run nginx container in detached mode to prevent provisioner blocking

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "digitalocean_droplet" "web" {
       "sudo apt-get install -y python3 docker.io docker-compose",
       "mkdir app",
       "docker login ghcr.io -u ${var.github_actor} -p ${var.github_token}",
-      "docker run -p 80:80 nginxdemos/hello"
+      "docker run -d -p 80:80 nginxdemos/hello"
     ]
   }
 }
