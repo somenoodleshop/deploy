ab05d0a49bf1d400b8cb8bc0fef1a7be92402ddf
arandombit <a.random.luis@proton.me>
2025-12-17 02:24:00 -0500

    skip region validation

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check = true
     skip_requesting_account_id = true
-    # skip_region_validation = true
+    skip_region_validation = true
   }
 }
 
