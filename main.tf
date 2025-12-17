a4aa7ac120094dcdd9e537d73d24e95091821f20
arandombit <a.random.luis@proton.me>
2025-12-16 23:18:08 -0500

    remove unnecessary S3 backend options

--- main.tf
+++ main.tf
@@ -16,12 +16,11 @@ terraform {
     endpoints = { s3 = "https://nyc3.digitaloceanspaces.com" }
     # endpoint = "https://nyc3.digitaloceanspaces.com"
     encrypt = true
-    #force_path_style = true
-    use_path_style = true
+    # force_path_style = true
     skip_credentials_validation = true
     skip_metadata_api_check = true
     skip_requesting_account_id = true
-    skip_region_validation = true
+    # skip_region_validation = true
   }
 }
 
