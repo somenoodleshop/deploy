b6e145fb529b14ca02fc05f6d8816e1f676ccce3
arandombit <a.random.luis@proton.me>
2025-12-17 02:38:07 -0500

    remove unnecessary comments from terraform file

--- main.tf
+++ main.tf
@@ -14,9 +14,7 @@ terraform {
     key = "terraform.tfstate"
     region = "us-east-1"
     endpoints = { s3 = "https://nyc3.digitaloceanspaces.com" }
-    # endpoint = "https://nyc3.digitaloceanspaces.com"
     encrypt = true
-    # force_path_style = true
     skip_credentials_validation = true
     skip_metadata_api_check = true
     skip_requesting_account_id = true
